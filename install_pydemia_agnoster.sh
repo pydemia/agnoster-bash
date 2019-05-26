4ba4dd4e563ab70e59dc2207ae77debb53de3046
Young Ju Kim <pydemia@gmail.com>
2019-05-27 00:55:37 +0900

    Update install_pydemia_agnoster.sh

--- install_pydemia_agnoster.sh
+++ install_pydemia_agnoster.sh
@@ -1,14 +1,40 @@
 mkdir -p ~/.bash/themes/agnoster-bash
 git clone https://github.com/pydemia/agnoster-bash.git ~/.bash/themes/agnoster-bash
 
-echo "
+
+
+script_str="
 # Theme for bash: agnoster-bash
 export THEME=$HOME/.bash/themes/agnoster-bash/agnoster.bash
 if [[ -f $THEME ]]; then
     export DEFAULT_USER=`whoami`
     source $THEME
 fi
-" >> ~/.bashrc
+"
+# $SHELL, $OSTYPE, $(uname -s)
+shell_name="$(basename $SHELL)"
+if [[ shell_name == "zsh" ]]; then
+    script_str >> ~/.zshrc
+elif [[ shell_name == "bash" ]]; then
+    script_str >> ~/.bashrc
+    if [ shell_name != "root" ] && [ -f "~/.bash_profile" ]; then
+        script_str >> ~/.bash_profile
+    fi
+# elif [[ "$OSTYPE" == "freebsd"* ]]; then
+#    script_str >> ~/.bash_profile
+# elif [[ "$OSTYPE" == "freebsd"* ]]; then
+#    script_str >> ~/.bash_profile
+# elif [[ "$OSTYPE" == "cygwin" ]]; then
+#         # POSIX compatibility layer and Linux environment emulation for Windows
+# elif [[ "$OSTYPE" == "msys" ]]; then
+#         # Lightweight shell and GNU utilities compiled for Windows (part of MinGW)
+# elif [[ "$OSTYPE" == "win32" ]]; then
+#         # I'm not sure this can happen.
+# elif [[ "$OSTYPE" == "freebsd"* ]]; then
+#         # ...
+else
+        # Unknown.
+fi
 
 
 
